38c2b33b854f4fa322c408291ed3fc05a479afa8
Amber Olsen <amcmains@ucsd.edu>
2020-02-02 18:54:21 -0800

    working on nav bar

---
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/Routes.java
@@ -0,0 +1,50 @@
+package com.cse110team24.walkwalkrevolution;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class Routes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routes2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+//        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                switch(menuItem.getItemId()) {
+//                    case R.id.action_home:
+//                    Toast.makeText(Routes.this, "Home", Toast.LENGTH_SHORT);
+//                    break;
+//                }
+//                return true;
+//            }
+//        });
+    }
+
+}
