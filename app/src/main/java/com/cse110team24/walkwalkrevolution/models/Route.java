654a41a94b7670d3a297c412df8ce220bdb6c68f
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-02-02 23:41:17 -0800

    create Route model

---
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/models/Route.java
@@ -0,0 +1,11 @@
+package com.cse110team24.walkwalkrevolution.models;
+
+import java.io.Serializable;
+import java.lang.Comparable;
+
+public class Route implements Serializable, Comparable {
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
