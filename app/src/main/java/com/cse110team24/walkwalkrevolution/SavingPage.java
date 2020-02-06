3da292d3a27e364d74fedc418f416d3c2e5dc205
Huanchao Shen <1439563937@qq.com>
2020-02-05 17:31:43 -0800

    build fixed?

--- app/src/main/java/com/cse110team24/walkwalkrevolution/SavingPage.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/SavingPage.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class SavingPage extends AppCompatActivity {
 
@@ -19,14 +21,30 @@ public class SavingPage extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+        /*TextInputEditText titleEditText = findViewById(R.id.titleEditText);
+        TextInputEditText titleEditText = findViewById(R.id.startingLocationEditText);
+        RadioGroup*/
+
+
+        /*Button doneButton = (Button) findViewById(R.id.doneButton);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if()
+
+                finish();
+            }
+        });*/
     }
 
 }
