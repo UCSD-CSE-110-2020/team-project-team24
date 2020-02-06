013704691e018b48a60d68ce7036a272af9f99a8
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-02-05 17:58:50 -0800

    add test cases for new r/w methods

--- app/src/test/java/com/cse110team24/walkwalkrevolution/RoutesManagerTest.java
+++ app/src/test/java/com/cse110team24/walkwalkrevolution/RoutesManagerTest.java
@@ -25,7 +25,8 @@ import static org.junit.Assert.fail;
 
 @RunWith(AndroidJUnit4.class)
 public class RoutesManagerTest {
-    private static final String TEST_FILE = ".WWR_storage_test";
+    private static final String TEST_FILE_LIST = ".WWR_storage_test_list";
+    private static final String TEST_FILE_SINGLE = ".WWR_storage_test_single";
 
     @Rule
     public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
@@ -38,20 +39,20 @@ public class RoutesManagerTest {
     }
 
     @Test
-    public void testRouteTitleOnly() {
+    public void testRouteTitleOnlyList() {
         scenario.onActivity(activity -> {
             Context context = activity.getApplicationContext();
             List<Route> routes = new ArrayList<>();
             String title = "Test title";
             routes.add(new Route(title));
             try {
-                RoutesManager.writeList(routes, TEST_FILE, context);
+                RoutesManager.writeList(routes, TEST_FILE_LIST, context);
             } catch (IOException e) {
                 fail();
             }
 
             try {
-                List<Route> readRoutes = RoutesManager.readList(TEST_FILE, context);
+                List<Route> readRoutes = RoutesManager.readList(TEST_FILE_LIST, context);
                 assertEquals(new Route(title), readRoutes.get(0));
             } catch (IOException e) {
                 fail();
@@ -60,33 +61,79 @@ public class RoutesManagerTest {
     }
 
     @Test
-    public void testAllFields() {
+    public void testAllFieldsList() {
         scenario.onActivity(activity -> {
-            RouteEnvironment env = getEnvironment();
-            WalkStats stats = new WalkStats(500, 100_000, 1.2, new GregorianCalendar());
-            Route route = new Route("title")
-                    .setStartingLocation("Test World")
-                    .setStats(stats)
-                    .setEnvironment(env)
-                    .setFavorite(true)
-                    .setNotes("Testing reading a route");
+            Route route = getRoute();
             List<Route> routes = new ArrayList<>();
 
             Context context = activity.getApplicationContext();
             routes.add(route);
             try {
-                RoutesManager.writeList(routes, TEST_FILE, context);
+                RoutesManager.writeList(routes, TEST_FILE_LIST, context);
             } catch (IOException e) {
                 fail();
             }
 
             try {
-                List<Route> readRoutes = RoutesManager.readList(TEST_FILE, context);
+                List<Route> readRoutes = RoutesManager.readList(TEST_FILE_LIST, context);
                 assertEquals(route, readRoutes.get(0));
             } catch (IOException e) {
                 fail();
             }
         });
+    }
+
+    @Test
+    public void testRouteTitleOnlySigle() {
+        scenario.onActivity(activity -> {
+            Context context = activity.getApplicationContext();
+            String title = "Test title";
+            Route route = new Route(title);
+            try {
+                RoutesManager.writeSingle(route, TEST_FILE_SINGLE, context);
+            } catch (IOException e) {
+                fail();
+            }
+
+            try {
+                Route readRoute = RoutesManager.readSingle(TEST_FILE_SINGLE, context);
+                assertEquals(new Route(title), readRoute);
+            } catch (IOException e) {
+                fail();
+            }
+        });
+    }
+
+    @Test
+    public void testAllFieldsSingle() {
+        scenario.onActivity(activity -> {
+            Route route = getRoute();
+
+            Context context = activity.getApplicationContext();
+            try {
+                RoutesManager.writeSingle(route, TEST_FILE_SINGLE, context);
+            } catch (IOException e) {
+                fail();
+            }
+
+            try {
+                Route readRoute = RoutesManager.readSingle(TEST_FILE_SINGLE, context);
+                assertEquals(route, readRoute);
+            } catch (IOException e) {
+                fail();
+            }
+        });
+    }
+
+    private Route getRoute() {
+        RouteEnvironment env = getEnvironment();
+        WalkStats stats = new WalkStats(500, 100_000, 1.2, new GregorianCalendar());
+        return new Route("title")
+                .setStartingLocation("Test World")
+                .setStats(stats)
+                .setEnvironment(env)
+                .setFavorite(true)
+                .setNotes("Testing reading a route");
     }
 
     private RouteEnvironment getEnvironment() {
