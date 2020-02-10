f4df4919c6207fff9bd6c60ee1d4b2fd6483466b
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-02-09 19:18:27 -0800

    check if time can be parsed

--- app/src/main/java/com/cse110team24/walkwalkrevolution/MockActivity.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/MockActivity.java
@@ -2,16 +2,27 @@ package com.cse110team24.walkwalkrevolution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.DateInterval;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.text.ParseException;
+
 public class MockActivity extends AppCompatActivity {
     public static final String START_WALK_BTN_VISIBILITY = "start button";
+    public static final String ADDED_STEPS_KEY = "added_steps";
+    public static final String INPUT_TIME_KEY = "input_time";
     public static final int REQUEST_CODE = 6;
+
     private static final long ADD_MOCK_CONST = 500;
+    private static final String TIME_FMT = "HH:mm:ss";
 
     private long totalAddedSteps;
+    private String timeSelected;
 
     private Button stepsMockBtn;
     
@@ -33,5 +44,23 @@ public class MockActivity extends AppCompatActivity {
         stepsMockBtn.setOnClickListener(view -> {
             totalAddedSteps += ADD_MOCK_CONST;
         });
+    }
+
+    private void finishMockAcitivity() {
+        Intent intent = new Intent()
+                .putExtra(ADDED_STEPS_KEY, totalAddedSteps)
+                .putExtra(INPUT_TIME_KEY, timeSelected);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
+
+    private boolean validateTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat(TIME_FMT);
+        try {
+            sdf.parse(timeSelected);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
     }
 }
