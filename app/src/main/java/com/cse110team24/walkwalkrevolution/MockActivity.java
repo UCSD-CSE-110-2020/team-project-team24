560e0a514a37ba9327e265cf86417a76e5a2e7c1
Daniel Sarafian <dsarafia@ucsd.edu>
2020-02-09 18:46:31 -0800

    add ability to mock steps with button

--- app/src/main/java/com/cse110team24/walkwalkrevolution/MockActivity.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/MockActivity.java
@@ -3,14 +3,35 @@ package com.cse110team24.walkwalkrevolution;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MockActivity extends AppCompatActivity {
     public static final String START_WALK_BTN_VISIBILITY = "start button";
     public static final int REQUEST_CODE = 6;
+    private static final long ADD_MOCK_CONST = 500;
+
+    private long totalAddedSteps;
+
+    private Button stepsMockBtn;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mock);
+
+        getUIFields();
+
+        setStepsMockOnClickListener();
+    }
+
+    private void getUIFields() {
+        stepsMockBtn = findViewById(R.id.btn_increment_steps);
+        //Enter Time
+    }
+    private void setStepsMockOnClickListener() {
+        stepsMockBtn.setOnClickListener(view -> {
+            totalAddedSteps += ADD_MOCK_CONST;
+        });
     }
 }
