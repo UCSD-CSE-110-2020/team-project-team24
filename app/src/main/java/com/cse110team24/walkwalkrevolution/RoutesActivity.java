2c5e0518012e886ce0e6df35a585893a0c19b991
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-02-11 15:47:38 -0800

    check for existing routes

--- app/src/main/java/com/cse110team24/walkwalkrevolution/RoutesActivity.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/RoutesActivity.java
@@ -1,5 +1,6 @@
 package com.cse110team24.walkwalkrevolution;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.cse110team24.walkwalkrevolution.models.Route;
@@ -8,22 +9,32 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.MenuItem;
-import android.view.View;
+import android.util.Log;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.List;
 
 public class RoutesActivity extends AppCompatActivity {
+    public static final String TAG = "RoutesActivity";
+    public static final int TO_REQUEST_CODE = 9;
 
-    ArrayList<Route> routes = new ArrayList<>();
+    public static final String LIST_SAVE_FILE = ".WWR_route_list_data";
+
+    RouteAdapter adapter;
+    RecyclerView rvRoutes;
+    FloatingActionButton fab;
+    BottomNavigationView bottomNavigationView;
+
+    List<Route> routes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,52 +43,81 @@ public class RoutesActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch(menuItem.getItemId()) {
-                    case R.id.action_home:
-                        launchGoToHomeActivity();
-                        break;
-
-                    case R.id.action_routes_list:
-                        break;
-                }
-                return true;
-            }
-        });
+        getUIElements();
+        setListeners();
 
         // Fake user routes info (needs to be deleted later
-        RecyclerView rvRoutes = findViewById(R.id.recycler_view);
         Route rt = new Route("Rose Canyon");
         Route rt2 = new Route("Marian Bear");
         Calendar calendar = Calendar.getInstance();
         calendar.set(2066,5,6);
         WalkStats ws = new WalkStats(22, 33, 0.01, calendar);
         rt.setStats(ws);
-
         routes.add(rt);
         routes.add(rt2);
         Collections.sort(routes);
-        RouteAdapter adapter = new RouteAdapter(routes, this);
+
+        adapter = new RouteAdapter(routes, this);
         rvRoutes.setAdapter(adapter);
         rvRoutes.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        checkForExistingSavedRoutes();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 
     public void launchGoToHomeActivity() {
         finish();
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+    }
+
+    private void getUIElements() {
+        fab = findViewById(R.id.fab);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        rvRoutes = findViewById(R.id.recycler_view);
+
+    }
+
+    private void setListeners() {
+        setFabOnClickListener();
+        setBottomNavItemSelectedListener();
+    }
+
+    private void setFabOnClickListener() {
+        fab.setOnClickListener(view -> {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        });
+    }
+
+    private void setBottomNavItemSelectedListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
+            switch(menuItem.getItemId()) {
+                case R.id.action_home:
+                    launchGoToHomeActivity();
+                    break;
+
+                case R.id.action_routes_list:
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void checkForExistingSavedRoutes() {
+        try {
+            List<Route> tempList = RoutesManager.readList(LIST_SAVE_FILE, this);
+            routes.addAll(tempList);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "checkForExistingSavedRoutes: no routes found ", e);
+        }
+        adapter.notifyDataSetChanged();
+        Log.i(TAG, "checkForExistingSavedRoutes: list of saved routes found with size " + routes.size());
     }
 
 }
