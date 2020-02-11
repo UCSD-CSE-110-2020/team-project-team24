eef504b8d8f12fc6704685810dc9dc55cdcb6f1a
Juan Dominguez <37827889+j5doming@users.noreply.github.com>
2020-02-11 14:41:08 -0800

    Mock Steps and Time (#56)
    * add ability to mock steps with button
    
    * add view to see total mocked steps
    
    * implement user interaction on Mock screen
    
    * keep mocked step count if already mocking
    
    * add mocking methods to FitnessService interface
    
    * make google fit adapter acknowledge mock values
    
    * rename layout ids for Height
    
    * reset time to system time after stopping walk
    
    * wrap around to next day if necessary
    
    * add toast to ask for end time
    
    * add unit tests for new unit in WalkStats
    
    * add Mock espresso test
    
    * add MockActivity unit tests
    
    * update HomeActivity unit tests
    
    Co-authored-by: dsarafian <38958368+dsarafian@users.noreply.github.com>

---
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/fitness/MockFitAdapter.java
@@ -0,0 +1,82 @@
+package com.cse110team24.walkwalkrevolution.fitness;
+
+import android.util.Log;
+
+import com.cse110team24.walkwalkrevolution.HomeActivity;
+
+public class MockFitAdapter implements FitnessService {
+    private static final String TAG = "MockFitAdapter";
+    private static final long HOURS_MILLIS = 86_400_000;
+
+    public static final String MOCK_SERVICE_KEY = "Mock_Fit";
+
+    private HomeActivity activity;
+    private long updatedSteps;
+    private long startSteps;
+    private long startTime; 
+    private long endTime; 
+
+    public MockFitAdapter(HomeActivity activity) {
+        this.activity = activity;
+    }
+ 
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setDailySteps(long updatedSteps) {
+        this.updatedSteps = updatedSteps;
+    }
+
+    @Override
+    public int getRequestCode() {
+        return 1;
+    }
+
+    @Override
+    public void setup() {
+    }
+
+    @Override
+    public void updateDailyStepCount() {
+        activity.setDailyStats(updatedSteps);
+    }
+
+    @Override
+    public void startRecording() {
+        startSteps = updatedSteps;
+    }
+
+    @Override
+    public void stopRecording() {
+        long timeElapsed = endTime - startTime;
+        if (timeElapsed < 0) {
+            timeElapsed += HOURS_MILLIS;
+        }
+        activity.setLatestWalkStats(updatedSteps - startSteps, timeElapsed);
+    }
+
+    @Override
+    public double getDistanceFromHeight(long steps, int heightFeet, float heightRemainderInches) {
+        return new GoogleFitAdapter(null).getDistanceFromHeight(steps, heightFeet, heightRemainderInches);
+    }
+
+    @Override
+    public void setStartRecordingTime(long startTime) {
+
+    }
+
+    @Override
+    public void setEndRecordingTime(long startTime) {
+
+    }
+
+    @Override
+    public void setStepsToAdd(long stepsToAdd) {
+
+    }
+}
