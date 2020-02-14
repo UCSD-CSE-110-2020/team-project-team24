f79b3b1b85dc291434efe5f65f8ae19bc64225bd
Huanchao Shen <1439563937@qq.com>
2020-02-14 14:48:39 -0800

    added setup for RouteDetailsActivityUnitTest

--- app/src/test/java/com/cse110team24/walkwalkrevolution/RouteDetailsActivityUnitTest.java
+++ app/src/test/java/com/cse110team24/walkwalkrevolution/RouteDetailsActivityUnitTest.java
@@ -1,5 +1,35 @@
 package com.cse110team24.walkwalkrevolution;
 
-public class RouteDetailsActivityUnitTest {
+import android.content.Intent;
 
+import androidx.test.core.app.ApplicationProvider;
+
+import com.cse110team24.walkwalkrevolution.models.Route;
+import com.cse110team24.walkwalkrevolution.models.RouteEnvironment;
+import com.cse110team24.walkwalkrevolution.models.WalkStats;
+
+import org.junit.Before;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class RouteDetailsActivityUnitTest {
+    private Intent intent;
+    private WalkStats stats;
+    private Route expectedRoute;
+    private Calendar date = new GregorianCalendar(2020, 2, 14);
+
+
+    @Before
+    public void setup() {
+        stats = new WalkStats(1500, 1800000, 0.82, date);
+        expectedRoute = new Route("Test Title")
+                .setStartingLocation("")
+                .setEnvironment(new RouteEnvironment())
+                .setNotes("")
+                .setStats(stats);
+        intent = new Intent(ApplicationProvider.getApplicationContext(), RouteDetailsActivity.class);
+        intent.putExtra(RouteDetailsActivity.ROUTE_KEY, expectedRoute);
+        intent.putExtra(RouteDetailsActivity.ROUTE_IDX_KEY, 0);
+    }
 }
