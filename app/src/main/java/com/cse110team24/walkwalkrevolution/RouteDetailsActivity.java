8b07a15832565b07ab64c7d2deb54846fa320d1a
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-02-13 17:25:30 -0800

    display latest stats if they exist

--- app/src/main/java/com/cse110team24/walkwalkrevolution/RouteDetailsActivity.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/RouteDetailsActivity.java
@@ -3,17 +3,74 @@ package com.cse110team24.walkwalkrevolution;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.cse110team24.walkwalkrevolution.models.Route;
+import com.cse110team24.walkwalkrevolution.models.WalkStats;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class RouteDetailsActivity extends AppCompatActivity {
     public static final String ROUTE_KEY = "route";
 
-    Route displayedRoute;
+    private Route displayedRoute;
+    private WalkStats stats;
+
+    private TextView recentStepsTv;
+    private TextView recentDistanceTv;
+    private TextView recentTimeElapsedTv;
+    private TextView neverWalkedPromptTv;
+    private Button startWalkBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_details);
+
+        displayedRoute = (Route) getIntent().getSerializableExtra(ROUTE_KEY);
+        stats = displayedRoute.getStats();
+        findUIElements();
+        checkRouteCompletion();
     }
+
+    private void findUIElements() {
+        recentStepsTv = findViewById(R.id.tv_details_recent_steps);
+        recentDistanceTv = findViewById(R.id.tv_details_recent_distance);
+        recentTimeElapsedTv = findViewById(R.id.tv_details_recent_time_elapsed);
+        neverWalkedPromptTv = findViewById(R.id.tv_details_never_walked);
+        startWalkBtn = findViewById(R.id.btn_details_start_walk);
+        setStartWalkBtnOnClickListener();
+    }
+
+    private void checkRouteCompletion() {
+        if (stats == null) {
+            neverWalkedPromptTv.setVisibility(View.VISIBLE);
+        } else {
+            displayLatestWalkStats();
+        }
+    }
+
+    private void displayLatestWalkStats() {
+        recentStepsTv.setText(String.valueOf(stats.getSteps()));
+        String distance = formattedDistanceTime(stats.getDistance(), "mile(s)");
+        recentDistanceTv.setText(distance);
+        String timeElapsed = formattedDistanceTime(stats.timeElapsedInMinutes(), "min.");
+        recentTimeElapsedTv.setText(timeElapsed);
+    }
+
+    private String formattedDistanceTime(double val, String suffix) {
+        NumberFormat numberFormat = new DecimalFormat("#0.00");
+        String formattedVal = numberFormat.format(val);
+        return String.format("%s %s", formattedVal, suffix);
+    }
+
+    private void setStartWalkBtnOnClickListener() {
+        startWalkBtn.setOnClickListener(view -> {
+
+        });
+    }
+
 }
