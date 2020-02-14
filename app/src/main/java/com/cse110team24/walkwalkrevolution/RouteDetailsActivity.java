39e15d2b25efffdbd720efd40b89b9c63f6ef89b
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-02-13 17:10:48 -0800

    configure primitive details screen

--- app/src/main/java/com/cse110team24/walkwalkrevolution/RouteDetailsActivity.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/RouteDetailsActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.cse110team24.walkwalkrevolution.models.Route;
+
 public class RouteDetailsActivity extends AppCompatActivity {
     public static final String ROUTE_KEY = "route";
+
+    Route displayedRoute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
