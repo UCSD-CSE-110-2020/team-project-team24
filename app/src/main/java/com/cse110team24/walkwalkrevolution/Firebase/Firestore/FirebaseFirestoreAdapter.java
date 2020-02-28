cb5beea2710ad9e619fff1eec177574d6bd52d37
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-02-27 22:36:22 -0800

    constants for firestore docs and collections

--- app/src/main/java/com/cse110team24/walkwalkrevolution/Firebase/Firestore/FirebaseFirestoreAdapter.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/Firebase/Firestore/FirebaseFirestoreAdapter.java
@@ -1,5 +1,7 @@
 package com.cse110team24.walkwalkrevolution.Firebase.Firestore;
 
 public class FirebaseFirestoreAdapter {
-
+    public static final String USERS_COLLECTION_KEY = "users";
+    public static final String ROUTES_COLLECTION_KEY = "routes";
+    public static final String TEAM_COLLECTION_KEY = "team";
 }
