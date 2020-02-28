5ec791e8c84b772b0296ebcbe4d9e4e3e8ebf038
jam124 <jam124@ucsd.edu>
2020-02-27 21:09:34 -0800

    Added the  UI for add Team Member

---
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/InviteTeamMember.java
@@ -0,0 +1,13 @@
+package com.cse110team24.walkwalkrevolution;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InviteTeamMember extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_invite_member);
+    }
+}
