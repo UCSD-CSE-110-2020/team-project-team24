ae6a739df0b6a6d914bad1f680367d809f552d19
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-02-29 15:22:01 -0800

    use builder for FirebaseAuthAdapter

--- app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/auth/FirebaseAuthAdapter.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/auth/FirebaseAuthAdapter.java
@@ -4,28 +4,31 @@ import android.app.Activity;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.cse110team24.walkwalkrevolution.models.user.FirebaseUserAdapter;
 import com.cse110team24.walkwalkrevolution.models.user.IUser;
+import com.cse110team24.walkwalkrevolution.models.user.FirebaseUserAdapter.FirebaseUserAdapterBuilder;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FirebaseAuthAdapter implements AuthService {
     private static String TAG = "FirebaseAuthAdapter";
     private static String USER_COLLISION = "ERROR: a user with this email already exists";
 
     private FirebaseAuth mAuth;
-    private FirebaseUserAdapter mUser;
+    private FirebaseUser mFirebaseUser;
+    private FirebaseUserAdapterBuilder mUserAdapterBuilder;
     private Activity mActivity;
     private AuthError mError;
 
+    // TODO: 2/29/20 user builder to create user
     public FirebaseAuthAdapter(Activity activity) {
         mAuth = FirebaseAuth.getInstance();
         mActivity = activity;
-        mUser = new FirebaseUserAdapter(mAuth.getCurrentUser());
+        mUserAdapterBuilder = new FirebaseUserAdapterBuilder();
     }
 
     @Override
@@ -34,18 +37,19 @@ public class FirebaseAuthAdapter implements AuthService {
                 .addOnCompleteListener(mActivity, task -> {
                     if (task.isSuccessful()) {
                         Log.i(TAG, "onComplete: user sign-in successful");
-                        mUser.setFirebaseUser(mAuth.getCurrentUser());
+                        buildUserEssentials(email);
                     } else {
                         Log.e(TAG, "signUp: user sign-in failed", task.getException());
                         Toast.makeText(mActivity, task.getException().toString(), Toast.LENGTH_LONG).show();
                         Toast.makeText(mActivity, "User is not logged in and data will not be saved", Toast.LENGTH_SHORT).show();
                         handleError(task);
-                        mUser = null;
                     }
                 });
 
-        return mUser;
+        return mUserAdapterBuilder.build();
     }
+
+
 
     @Override
     public IUser signUp(String email, String password) {
@@ -53,14 +57,37 @@ public class FirebaseAuthAdapter implements AuthService {
                 .addOnCompleteListener(mActivity, task -> {
                     if (task.isSuccessful()) {
                         Log.i(TAG, "signUp: user creation successful");
-                        mUser.setFirebaseUser(mAuth.getCurrentUser());
+                        buildUserEssentials(email);
                     } else {
                         Log.e(TAG, "signUp: user creation failed", task.getException());
                         handleError(task);
-                        mUser = null;
                     }
                 });
-        return mUser;
+        return mUserAdapterBuilder.build();
+    }
+
+    @Override
+    public IUser signUpWithName(String email, String password, String displayName) {
+        signUp(email, password);
+        if (isUserSignedIn()) {
+            mUserAdapterBuilder.addDisplayName(displayName);
+        }
+        return mUserAdapterBuilder.build();
+    }
+
+    @Override
+    public IUser getUser() {
+        return mUserAdapterBuilder.build();
+    }
+
+    @Override
+    public boolean isUserSignedIn() {
+        return mFirebaseUser != null;
+    }
+
+    @Override
+    public AuthError getAuthError() {
+        return mError;
     }
 
     private void handleError(Task<AuthResult> task) {
@@ -75,27 +102,9 @@ public class FirebaseAuthAdapter implements AuthService {
         }
     }
 
-    @Override
-    public IUser signUpWithName(String email, String password, String displayName) {
-        signUp(email, password);
-        if (isUserSignedIn()) {
-            mUser.updateDisplayName(displayName);
-        }
-        return mUser;
-    }
-
-    @Override
-    public IUser getUser() {
-        return mUser;
-    }
-
-    @Override
-    public boolean isUserSignedIn() {
-        return mUser != null && mUser.firebaseUser() != null;
-    }
-
-    @Override
-    public AuthError getAuthError() {
-        return mError;
+    private void buildUserEssentials(String email) {
+        mUserAdapterBuilder.addEmail(email);
+        mFirebaseUser = mAuth.getCurrentUser();
+        mUserAdapterBuilder.addFirebaseUser(mFirebaseUser);
     }
 }
