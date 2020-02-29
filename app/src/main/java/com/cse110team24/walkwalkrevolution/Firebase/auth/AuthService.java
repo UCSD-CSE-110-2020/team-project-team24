90d18106364a3cbee5d1ed43ccd3899b7ac4b7ed
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-02-29 09:45:46 -0800

    convenience method for signing up

--- app/src/main/java/com/cse110team24/walkwalkrevolution/Firebase/auth/AuthService.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/Firebase/auth/AuthService.java
@@ -5,6 +5,7 @@ import com.cse110team24.walkwalkrevolution.models.user.IUser;
 public interface AuthService {
     IUser signIn(String email, String password);
     IUser signUp(String email, String password);
+    IUser signUpWithName(String email, String password, String displayName);
     IUser getUser();
     boolean isUserSignedIn();
 }
