a451215b4316b6a7448e9d8afdd75543aa6ebdcc
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-03-01 13:18:17 -0800

    custom subject interface for messaging service

--- app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/messaging/MessagingService.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/messaging/MessagingService.java
@@ -3,7 +3,7 @@ package com.cse110team24.walkwalkrevolution.firebase.messaging;
 import com.cse110team24.walkwalkrevolution.models.invitation.Invitation;
 import com.cse110team24.walkwalkrevolution.observer.Subject;
 
-public interface MessagingService extends Subject<MessagingObserver> {
+public interface MessagingService extends MessagingSubject {
 
     void subscribeToNotificationsTopic(String topic);
     void sendInvitation(Invitation invitation);
---
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/messaging/MessagingSubject.java
@@ -0,0 +1,10 @@
+package com.cse110team24.walkwalkrevolution.firebase.messaging;
+
+import com.cse110team24.walkwalkrevolution.models.invitation.Invitation;
+import com.cse110team24.walkwalkrevolution.observer.Subject;
+import com.google.android.gms.tasks.Task;
+
+public interface MessagingSubject extends Subject<MessagingObserver> {
+    void notifyObserversInvitationSent(Invitation invitation);
+    void notifyObserversFailedInvitationSent(Task<?> task);
+}
