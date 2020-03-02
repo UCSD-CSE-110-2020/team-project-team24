dc864525abd0f132a988788c9f817c76f556a63c
jam124 <jam124@ucsd.edu>
2020-03-02 15:01:14 -0800

    Last few tweaks to the UI of walking team

--- app/src/main/java/com/cse110team24/walkwalkrevolution/TeamPage.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/TeamPage.java
@@ -1,13 +1,61 @@
 package com.cse110team24.walkwalkrevolution;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class TeamPage extends Activity {
+
+    private Button sendInviteBtn;
+    private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_page);
+
+
+        getUIFields();
+        setButtonClickListeners();
+    }
+    private void getUIFields() {
+        sendInviteBtn = findViewById(R.id.btn_invite_team_members);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+    }
+    private void setButtonClickListeners() {
+        setInviteButtonOnClick();
+        // setBottomNavigationOnClick();
+        setBottomNavItemSelectedListener();
+    }
+
+    private void setInviteButtonOnClick() {
+        sendInviteBtn.setOnClickListener(view -> {
+            launchInviteRouteActivity();
+        });
+    }
+    private void setBottomNavItemSelectedListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
+            if(menuItem.getItemId() == R.id.action_home) {
+                Intent myIntent = new Intent(getApplicationContext(), HomeActivity.class);
+                myIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(myIntent);
+            }
+            if(menuItem.getItemId() == R.id.action_routes_list) {
+                Intent myIntent = new Intent(getApplicationContext(), RoutesActivity.class);
+                myIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(myIntent);
+            }
+
+            return true;
+        });
+    }
+    private void launchInviteRouteActivity() {
+        Intent intent = new Intent(this, InviteTeamMember.class);
+        startActivity(intent);
     }
 }
