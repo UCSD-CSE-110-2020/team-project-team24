e560028eae767171288ea0b4d5f908a3cb5fb274
Amber Olsen <amcmains@ucsd.edu>
2020-03-02 15:29:17 -0800

    working on login activity unit tests

--- app/src/test/java/com/cse110team24/walkwalkrevolution/LoginActivityUnitTest.java
+++ app/src/test/java/com/cse110team24/walkwalkrevolution/LoginActivityUnitTest.java
@@ -2,6 +2,7 @@ package com.cse110team24.walkwalkrevolution;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -11,8 +12,10 @@ import com.cse110team24.walkwalkrevolution.application.FirebaseApplicationWWR;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.shadows.ShadowToast;
 
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
@@ -20,8 +23,17 @@ public class LoginActivityUnitTest extends TestInjection {
 
     private LoginActivity testActivity;
     private Button finishBtn;
+    private TextView signUpTV;
     private EditText feetEt;
     private EditText inchesEt;
+    private EditText gmail;
+    private EditText username;
+    private EditText password;
+    private static final String TOAST_MSG_NO_EMAIL = "Please enter an email!";
+    private static final String TOAST_MSG_NO_PASSWORD = "Please enter your password!";
+    private static final String TOAST_MSG_NOT_GMAIL = "Please enter a valid gmail address!";
+    private static final String TOAST_MSG_NO_USERNAME = "Please enter your name!";
+    private static final String TOAST_MSG_USER_COLLISION = "user already exists!";
 
     @Before
     public void setup() {
@@ -36,22 +48,12 @@ public class LoginActivityUnitTest extends TestInjection {
             finishBtn = testActivity.findViewById(R.id.btn_height_finish);
             feetEt = testActivity.findViewById(R.id.et_height_feet);
             inchesEt = testActivity.findViewById(R.id.et_height_remainder_inches);
+            gmail = testActivity.findViewById(R.id.enter_gmail_address);
+            password = testActivity.findViewById(R.id.enter_password);
+            signUpTV = testActivity.findViewById(R.id.sign_up_tv);
+            username = testActivity.findViewById(R.id.enter_username);
         });
     }
-
-//    @Test
-//    public void testFinishBtnDisabledBtnOnStart() {
-//        assertTrue(!finishBtn.isEnabled());
-//    }
-
-//    @Test
-//    public void testFinishBtnDisabledBadText() {
-//        feetEt.setText("5");
-//        assertTrue(!finishBtn.isEnabled());
-//        feetEt.setText("");
-//        inchesEt.setText("3");
-//        assertTrue(!finishBtn.isEnabled());
-//    }
 
     @Test
     public void testFinishBtnEnabled() {
@@ -59,4 +61,69 @@ public class LoginActivityUnitTest extends TestInjection {
         inchesEt.setText("3");
         assertTrue(finishBtn.isEnabled());
     }
+
+    @Test
+    public void loginWithoutEmail() {
+        feetEt.setText("5");
+        inchesEt.setText("3");
+        finishBtn.performClick();
+        assertEquals(ShadowToast.getTextOfLatestToast(), TOAST_MSG_NO_EMAIL);
+    }
+
+    @Test
+    public void loginWithoutPassword() {
+        feetEt.setText("5");
+        inchesEt.setText("3");
+        gmail.setText("amber@gmail.com");
+        finishBtn.performClick();
+        assertEquals(ShadowToast.getTextOfLatestToast(),TOAST_MSG_NO_PASSWORD);
+    }
+
+    @Test
+    public void loginWithoutGmailAddress() {
+        feetEt.setText("5");
+        inchesEt.setText("3");
+        gmail.setText("amber@yahoo.com");
+        password.setText("testpw");
+        finishBtn.performClick();
+       // assertEquals(ShadowToast.getTextOfLatestToast(), TOAST_MSG_NOT_GMAIL);
+        //TODO !!
+    }
+
+    @Test
+    public void signUpWithoutGmailAddress() {
+        signUpTV.performClick();
+        feetEt.setText("5");
+        inchesEt.setText("3");
+        gmail.setText("amber@yahoo.com");
+        password.setText("testpw");
+        username.setText("Cheery");
+        finishBtn.performClick();
+        assertEquals(ShadowToast.getTextOfLatestToast(), TOAST_MSG_NOT_GMAIL);
+    }
+
+    @Test
+    public void signUpWithoutUsername() {
+        signUpTV.performClick();
+        feetEt.setText("5");
+        inchesEt.setText("3");
+        gmail.setText("amber@gmail.com");
+        password.setText("testpw");
+        finishBtn.performClick();
+        assertEquals(ShadowToast.getTextOfLatestToast(), TOAST_MSG_NO_USERNAME);
+    }
+
+//    @Test
+//    public void userCollision() {
+//        mAuth.signUp("amber@gmail.com", "testpw", "Cheery");
+//        feetEt.setText("5");
+//        inchesEt.setText("3");
+//        gmail.setText("amber@gmail.com");
+//        password.setText("testpw");
+//        username.setText("Cheery");
+//        finishBtn.performClick();
+//        assertEquals(ShadowToast.getTextOfLatestToast(), TOAST_MSG_USER_COLLISION);
+//    }
+
+
 }
