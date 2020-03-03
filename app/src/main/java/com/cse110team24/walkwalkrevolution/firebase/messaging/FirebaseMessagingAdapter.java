a35a34c1ee5c8f674cfa1d5b419a5b9dcc826421
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-03-03 12:03:14 -0800

    messaging adapter casts to correct db service

--- app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/messaging/FirebaseMessagingAdapter.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/messaging/FirebaseMessagingAdapter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.util.Log;
 
 import com.cse110team24.walkwalkrevolution.firebase.firestore.DatabaseService;
+import com.cse110team24.walkwalkrevolution.firebase.firestore.services.InvitationsDatabaseService;
 import com.cse110team24.walkwalkrevolution.models.invitation.Invitation;
 import com.cse110team24.walkwalkrevolution.models.invitation.InvitationStatus;
 import com.google.android.gms.tasks.Task;
@@ -48,10 +49,11 @@ public class FirebaseMessagingAdapter implements MessagingService {
 
     @Override
     public void sendInvitation(Invitation invitation) {
-        mDb.addInvitationForReceivingUser(invitation).addOnCompleteListener(task -> {
+        InvitationsDatabaseService db = (InvitationsDatabaseService) mDb;
+        db.addInvitationForReceivingUser(invitation).addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
                 Log.i(TAG, "sendInvitation: invitation sent successfully");
-                mDb.addInvitationForSendingUser(invitation);
+                db.addInvitationForSendingUser(invitation);
                 notifyObserversInvitationSent(invitation);
             } else {
                 Log.e(TAG, "sendInvitation: error sending invitation", task.getException());
