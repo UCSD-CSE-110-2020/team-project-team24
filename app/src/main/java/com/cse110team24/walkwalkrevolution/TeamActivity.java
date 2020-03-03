6e81ae63d4a6a04046f6f588f65a3f673c8f6ff8
Huanchao Shen <1439563937@qq.com>
2020-03-03 00:16:33 -0800

    TeamActivity UI integration implemented, needs testing

--- app/src/main/java/com/cse110team24/walkwalkrevolution/TeamActivity.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/TeamActivity.java
@@ -5,15 +5,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ListViewAutoScrollHelper;
 
 import com.cse110team24.walkwalkrevolution.application.FirebaseApplicationWWR;
 import com.cse110team24.walkwalkrevolution.firebase.firestore.DatabaseService;
 import com.cse110team24.walkwalkrevolution.firebase.firestore.DatabaseServiceObserver;
 import com.cse110team24.walkwalkrevolution.models.team.ITeam;
+import com.cse110team24.walkwalkrevolution.models.user.IUser;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.List;
 import java.util.Map;
 
 public class TeamActivity extends AppCompatActivity implements DatabaseServiceObserver {
@@ -94,6 +98,10 @@ public class TeamActivity extends AppCompatActivity implements DatabaseServiceOb
     @Override
     public void onTeamRetrieved(ITeam team) {
         mTeam = team;
+        List<IUser> users = mTeam.getTeam();
+        ListView teammatesList = (ListView) findViewById(R.id.list_members_in_team);
+        ListviewAdapter listviewAdapter = new ListviewAdapter(this, users);
+        teammatesList.setAdapter(listviewAdapter);
         // start updating UI
     }
 
