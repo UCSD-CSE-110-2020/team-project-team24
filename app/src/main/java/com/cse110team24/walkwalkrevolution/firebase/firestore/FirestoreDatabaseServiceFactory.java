7184287c5263495b4cd7ab509581abe4bbc943ea
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-03-03 11:58:09 -0800

    factory returns one of each type

--- app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/firestore/FirestoreDatabaseServiceFactory.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/firestore/FirestoreDatabaseServiceFactory.java
@@ -1,5 +1,7 @@
 package com.cse110team24.walkwalkrevolution.firebase.firestore;
 
+import com.cse110team24.walkwalkrevolution.firebase.firestore.adapters.FireBaseFireStoreAdapterTeams;
+import com.cse110team24.walkwalkrevolution.firebase.firestore.adapters.FirebaseFirestoreAdapterInvitations;
 import com.cse110team24.walkwalkrevolution.firebase.firestore.adapters.FirebaseFirestoreAdapterUsers;
 
 // TODO: 3/3/20 return one of three database services
@@ -10,11 +12,11 @@ public class FirestoreDatabaseServiceFactory implements DatabaseServiceFactory{
     public DatabaseService createDatabaseService(DatabaseService.Service service) {
         switch (service) {
             case USERS:
-                break;
+                return new FirebaseFirestoreAdapterUsers();
             case TEAMS:
-                break;
+                return new FireBaseFireStoreAdapterTeams();
             case INVITATIONS:
-                break;
+                return new FirebaseFirestoreAdapterInvitations();
         }
         return new FirebaseFirestoreAdapter();
     }
