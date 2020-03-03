4a6590550330051e98c94b85eabacf47bdbe8e03
Cheery <cheeryone821@gmail.com>
2020-03-03 13:12:47 -0800

    solve local conflict

--- app/src/test/java/com/cse110team24/walkwalkrevolution/InviteTeammateUnitTest.java
+++ app/src/test/java/com/cse110team24/walkwalkrevolution/InviteTeammateUnitTest.java
@@ -1,14 +1,20 @@
 package com.cse110team24.walkwalkrevolution;
 
+import android.content.Intent;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.cse110team24.walkwalkrevolution.firebase.messaging.MessagingService;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.robolectric.shadows.ShadowToast;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static junit.framework.TestCase.assertEquals;
@@ -17,18 +23,23 @@ import static junit.framework.TestCase.assertEquals;
 public class InviteTeammateUnitTest extends TestInjection {
 
     private InviteTeamMemberActivity testActivity;
+
     private Button sendInviteBtn;
     private TextView inviteNameTv;
     private TextView inviteEmailTv;
     private static final String TOAST_MSG_NOT_GMAIL = "Please enter a valid gmail address";
+
+    private static final String INVALID_EMAIL = "Please enter a valid email address";
 
     @Before
     public void setup() {
         super.setup();
 
         ActivityScenario<InviteTeamMemberActivity> scenario = ActivityScenario.launch(InviteTeamMemberActivity.class);
+
         scenario.onActivity(activity -> {
             testActivity = activity;
+
             sendInviteBtn = testActivity.findViewById(R.id.btn_send_invite);
             inviteNameTv = testActivity.findViewById(R.id.field_enter_member_name);
             inviteEmailTv = testActivity.findViewById(R.id.field_enter_member_email);
@@ -36,8 +47,10 @@ public class InviteTeammateUnitTest extends TestInjection {
     }
 
     @Test
-    public void emptyTest() {
-        // TODO: change the name and fill out this test
+    public void noInfoInvitation() {
+//        sendInviteBtn.performClick();
+//        assertEquals(ShadowToast.getTextOfLatestToast(), INVALID_EMAIL);
+
     }
 
     @Test
@@ -45,7 +58,6 @@ public class InviteTeammateUnitTest extends TestInjection {
         inviteNameTv.setText("Marian");
         sendInviteBtn.performClick();
         assertEquals(ShadowToast.getTextOfLatestToast(), TOAST_MSG_NOT_GMAIL);
-
     }
 
 }
