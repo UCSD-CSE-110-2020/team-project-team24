cb44efa104bebb39ed7ff890d79cb4ad0349c6b5
Jose Moreno <jam124@ucsd.edu>
2020-03-04 22:13:37 -0800

    Added a Unit Test class for US3 testing

---
+++ app/src/test/java/com/cse110team24/walkwalkrevolution/TeamActivityUnitTest.java
@@ -0,0 +1,29 @@
+package com.cse110team24.walkwalkrevolution;
+
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.cse110team24.walkwalkrevolution.models.route.Route;
+import com.cse110team24.walkwalkrevolution.models.route.RouteEnvironment;
+import com.cse110team24.walkwalkrevolution.models.route.WalkStats;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.shadows.ShadowToast;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class TeamActivityUnitTest {
+}
