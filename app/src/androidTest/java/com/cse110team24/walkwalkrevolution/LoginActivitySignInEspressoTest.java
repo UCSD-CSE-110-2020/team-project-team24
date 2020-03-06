f1f6c72069c2028316b9266d95d7b0161f708c80
Jose Moreno <jam124@ucsd.edu>
2020-03-05 20:04:47 -0800

    Added an Espresso test for Sigin

---
+++ app/src/androidTest/java/com/cse110team24/walkwalkrevolution/LoginActivitySignInEspressoTest.java
@@ -0,0 +1,81 @@
+package com.cse110team24.walkwalkrevolution;
+
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.cse110team24.walkwalkrevolution.fitness.FitnessServiceFactory;
+import com.cse110team24.walkwalkrevolution.mockedservices.MockedApplicationAndroidTestRunner;
+
+import com.cse110team24.walkwalkrevolution.mockedservices.MockActivityTestRule;
+import com.cse110team24.walkwalkrevolution.mockedservices.TestAuth;
+import com.cse110team24.walkwalkrevolution.mockedservices.TestFitnessService;
+import com.cse110team24.walkwalkrevolution.models.user.FirebaseUserAdapter;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.cse110team24.walkwalkrevolution.mockedservices.TestFitnessService.TEST_SERVICE_KEY;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginActivitySignInEspressoTest {
+
+    @Rule
+    public MockActivityTestRule<LoginActivity> mActivityTestRule = new MockActivityTestRule<>(LoginActivity.class);
+
+    @Before
+    public void setup() {
+        FitnessServiceFactory.put(TEST_SERVICE_KEY, activity -> new TestFitnessService(activity));
+        mActivityTestRule.getActivity().setFitnessServiceKey(TEST_SERVICE_KEY);
+        TestAuth.isTestUserSignedIn = true;
+        TestAuth.successUserSignedUp = true;
+        TestAuth.testAuthUser = FirebaseUserAdapter.builder()
+                .addDisplayName("Amara Momoh")
+                .addEmail("amara@gmail.com")
+                .addUid("1")
+                .addTeamUid("666")
+                .build();
+
+
+    }
+    @Test
+    public void loginActivitySignInEspressoTest() {
+        setup();
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.enter_gmail_address), isDisplayed()));
+        appCompatEditText.perform(replaceText("amara@gmail.com"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.enter_password), isDisplayed()));
+        appCompatEditText2.perform(replaceText("banana"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.et_height_feet), isDisplayed()));
+        appCompatEditText4.perform(replaceText("6"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.et_height_remainder_inches), isDisplayed()));
+        appCompatEditText5.perform(replaceText("1"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_height_finish), withText("Login"), isDisplayed()));
+        appCompatButton.perform(click());
+
+    }
+}
