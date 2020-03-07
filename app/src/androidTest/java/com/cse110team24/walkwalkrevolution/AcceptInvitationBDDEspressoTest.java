7fe389470cf1ffca482651eef6d9ab8f5fa2d602
Jose Moreno <jam124@ucsd.edu>
2020-03-06 19:14:40 -0800

    Created a BDD espresso test for accept invite then see your new team

--- app/src/androidTest/java/com/cse110team24/walkwalkrevolution/AcceptInvitationBDDEspressoTest.java
+++ app/src/androidTest/java/com/cse110team24/walkwalkrevolution/AcceptInvitationBDDEspressoTest.java
@@ -6,25 +6,39 @@ import com.cse110team24.walkwalkrevolution.fitness.FitnessServiceFactory;
 import com.cse110team24.walkwalkrevolution.mockedservices.MockActivityTestRule;
 import com.cse110team24.walkwalkrevolution.mockedservices.TestAuth;
 import com.cse110team24.walkwalkrevolution.mockedservices.TestFitnessService;
+import com.cse110team24.walkwalkrevolution.mockedservices.TestInvitationsDatabaseService;
 import com.cse110team24.walkwalkrevolution.mockedservices.TestMessage;
+import com.cse110team24.walkwalkrevolution.models.invitation.Invitation;
+import com.cse110team24.walkwalkrevolution.models.team.TeamAdapter;
 import com.cse110team24.walkwalkrevolution.models.user.FirebaseUserAdapter;
+import com.cse110team24.walkwalkrevolution.models.user.IUser;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.cse110team24.walkwalkrevolution.mockedservices.TestFitnessService.TEST_SERVICE_KEY;
+import static com.cse110team24.walkwalkrevolution.mockedservices.TestTeamsDatabaseService.testTeam;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
 
 public class AcceptInvitationBDDEspressoTest {
+
+    private List<IUser> listOfUsers;
+    private List<Invitation> invitationList;
+
     @Rule
     public MockActivityTestRule<LoginActivity> mActivityTestRule = new MockActivityTestRule<>(LoginActivity.class);
 
@@ -35,12 +49,44 @@ public class AcceptInvitationBDDEspressoTest {
         TestAuth.isTestUserSignedIn = true;
         TestAuth.successUserSignedIn = true;
         TestMessage.invitationSentSuccess = true;
-        TestAuth.testAuthUser = FirebaseUserAdapter.builder()
+
+        //ME
+        IUser satta_momoh = FirebaseUserAdapter.builder()
                 .addDisplayName("Amara Momoh")
                 .addEmail("amara@gmail.com")
                 .addUid("1")
+                .addTeamUid(null)
+                .build();
+
+        TestAuth.testAuthUser = satta_momoh;
+
+        listOfUsers = new ArrayList<IUser>();
+        invitationList = new ArrayList<Invitation>();
+        testTeam = new TeamAdapter(listOfUsers);
+
+        testTeam.addMember(satta_momoh);
+//PERSON INVITING ME
+        IUser amara_momoh = FirebaseUserAdapter.builder()
+                .addDisplayName("Amara Momoh")
+                .addEmail("ival@gmail.com")
+                .addUid("2")
                 .addTeamUid("666")
                 .build();
+        testTeam.addMember(amara_momoh);
+
+        Invitation sentInvitation = new Invitation(amara_momoh);
+
+        invitationList.add(sentInvitation);
+
+        IUser maria_momoh = FirebaseUserAdapter.builder()
+                .addDisplayName("Maria Momoh")
+                .addEmail("mars@gmail.com")
+                .addUid("3")
+                .addTeamUid("666")
+                .build();
+        testTeam.addMember(maria_momoh);
+
+        TestInvitationsDatabaseService.testInvitationsList = invitationList;
 
     }
     @Test
@@ -71,10 +117,18 @@ public class AcceptInvitationBDDEspressoTest {
                 allOf(withId(R.id.action_team), withContentDescription("Team"), isDisplayed()));
         bottomNavigationItemView.perform(click());
 
-  /*      ViewInteraction appCompatButton2 = onView(
+        ViewInteraction appCompatButton2 = onView(
                 allOf(withId(R.id.btn_pending_invites), withText("Pending Invites"), isDisplayed()));
         appCompatButton2.perform(click());
 
-   */
+        ViewInteraction listViewClick = onView(
+                allOf(withId(R.id.invitationList), withContentDescription("Amara Momoh"), isDisplayed()));
+        //listViewClick.perform(click());
+
+
+
+
+
+
     }
 }
