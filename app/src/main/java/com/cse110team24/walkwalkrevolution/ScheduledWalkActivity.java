8c504bda8adbf424f8a5ce52e6391de8ec77e6cf
Amber Olsen <amcmains@ucsd.edu>
2020-03-09 10:12:06 -0700

    starting ActivityScheduledWalk class

---
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/ScheduledWalkActivity.java
@@ -0,0 +1,14 @@
+package com.cse110team24.walkwalkrevolution;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScheduledWalkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
