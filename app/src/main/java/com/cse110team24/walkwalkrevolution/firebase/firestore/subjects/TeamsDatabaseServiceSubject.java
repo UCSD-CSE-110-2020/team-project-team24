9e56492585113920c050ca6df15427a91c9fe7ac
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-03-09 12:02:47 -0700

    fix docs for teams subject

--- app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/firestore/subjects/TeamsDatabaseServiceSubject.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/firebase/firestore/subjects/TeamsDatabaseServiceSubject.java
@@ -12,22 +12,25 @@ import java.util.List;
 
 public interface TeamsDatabaseServiceSubject extends Subject<TeamsDatabaseServiceObserver> {
     /**
-     * Notify this subject's observers that the requested team's teammates data is ready to read. 
-     * See also: {@link TeamsDatabaseServiceObserver#onTeamRetrieved(ITeam)}.
+     * Notify this subject's observers of type {@link com.cse110team24.walkwalkrevolution.firebase.firestore.observers.teams.TeamsTeammatesObserver}
+     * that the requested team's teammates data is ready to read.
+     * See also: {@link com.cse110team24.walkwalkrevolution.firebase.firestore.observers.teams.TeamsTeammatesObserver#onTeamRetrieved(ITeam)}.
      * @param team the team that was retrieved. Does not include the currently signed in user.
      */
     void notifyObserversTeamRetrieved(ITeam team);
 
     /**
-     * Notify this subject's observers that the requested team's routes data is ready to be read.
-     * <p>See also: {@link TeamsDatabaseServiceObserver#onRoutesRetrieved(List, DocumentSnapshot)}</p>
+     * Notify this subject's observers of type {@link com.cse110team24.walkwalkrevolution.firebase.firestore.observers.teams.TeamsRoutesObserver}
+     * that the requested team's routes data is ready to be read.
+     * <p>See also: {@link com.cse110team24.walkwalkrevolution.firebase.firestore.observers.teams.TeamsRoutesObserver#onRoutesRetrieved(List, DocumentSnapshot)}</p>
      * @param routes the list of routes that was retrieved. Does not include the currently signed in user's routes.
      * @param lastRoute the last route in the list's DocumentSnapshot.
      */
     void notifyObserversTeamRoutesRetrieved(List<Route> routes, DocumentSnapshot lastRoute);
 
     /**
-     * Notify this subject's observers that the requested team's walks data is ready to be read.
+     * Notify this subject's observers of type {@link TeamsTeamWalksObserver} that the requested
+     * team's walks data is ready to be read.
      * <p>See also: {@link TeamsTeamWalksObserver#onTeamWalksRetrieved(List)}</p>
      * @param walks the list of TeamWalks that was retrieved.
      */
