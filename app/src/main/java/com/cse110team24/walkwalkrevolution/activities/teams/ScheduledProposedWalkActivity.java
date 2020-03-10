bdfe15d66a1707d7adbc6b7c79eb1548b064db4d
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-03-10 15:12:47 -0700

    differentiate proposer from regulat teammate

--- app/src/main/java/com/cse110team24/walkwalkrevolution/activities/teams/ScheduledProposedWalkActivity.java
+++ app/src/main/java/com/cse110team24/walkwalkrevolution/activities/teams/ScheduledProposedWalkActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.cse110team24.walkwalkrevolution.HomeActivity;
@@ -17,22 +19,16 @@ import com.cse110team24.walkwalkrevolution.firebase.firestore.services.DatabaseS
 import com.cse110team24.walkwalkrevolution.firebase.firestore.services.TeamsDatabaseService;
 import com.cse110team24.walkwalkrevolution.firebase.firestore.services.UsersDatabaseService;
 import com.cse110team24.walkwalkrevolution.models.route.Route;
-import com.cse110team24.walkwalkrevolution.models.team.ITeam;
-import com.cse110team24.walkwalkrevolution.models.team.TeamAdapter;
 import com.cse110team24.walkwalkrevolution.models.team.walk.TeamWalk;
+import com.cse110team24.walkwalkrevolution.models.user.FirebaseUserAdapter;
 import com.cse110team24.walkwalkrevolution.models.user.IUser;
 import com.cse110team24.walkwalkrevolution.utils.Utils;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ScheduledProposedWalkActivity extends AppCompatActivity implements TeamsTeamWalksObserver {
     private static final String TAG = "WWR_ScheduledProposedWalkActivity";
 
-    private TextView walkName;
-    private TextView startingLocation;
-    private TextView walkDate;
     private Button acceptBtn;
     private Button declineCannotMakeItBtn;
     private Button declineNotInterestedBtn;
@@ -41,6 +37,8 @@ public class ScheduledProposedWalkActivity extends AppCompatActivity implements 
 
     private TeamsDatabaseService mDb;
     private UsersDatabaseService uDb;
+    private TeamWalk mCurrentTeamWalk;
+    private IUser mCurrentUser;
 
     SharedPreferences mPreferences;
 
@@ -56,7 +54,7 @@ public class ScheduledProposedWalkActivity extends AppCompatActivity implements 
         preferences = getSharedPreferences(HomeActivity.APP_PREF, Context.MODE_PRIVATE);
         setUpServices();
         getUIFields();
-        getTeamUid();
+        getCurrentUser();
     }
 
     @Override
@@ -73,14 +71,6 @@ public class ScheduledProposedWalkActivity extends AppCompatActivity implements 
     }
 
     private void getUIFields() {
-        walkName = findViewById(R.id.schedule_propose_tv_walk_name_display);
-        startingLocation = findViewById(R.id.schedule_propose_tv_starting_loc_display);
-        walkDate = findViewById(R.id.schedule_propose_tv_walk_date_display);
-        acceptBtn = findViewById(R.id.accept_btn);
-        declineCannotMakeItBtn = findViewById(R.id.decline_cant_come_btn);
-        declineNotInterestedBtn = findViewById(R.id.decline_not_interested_btn);
-        //scheduleWalkBtn;
-        //withdrawWalkBtn;
     }
 
     private void getTeamUid() {
@@ -91,12 +81,88 @@ public class ScheduledProposedWalkActivity extends AppCompatActivity implements 
         mDb.getUserTeam(mTeamUid, preferences.getString(IUser.USER_NAME_KEY, ""));
     }
 
+    private void getCurrentUser() {
+        getTeamUid();
+        mCurrentUser = FirebaseUserAdapter.builder()
+                .addDisplayName(mPreferences.getString(IUser.USER_NAME_KEY, ""))
+                .addTeamUid(mTeamUid)
+                .build();
+    }
+
     @Override
     public void onTeamWalksRetrieved(List<TeamWalk> teamWalks) {
-        Route proposedRoute = teamWalks.get(0).getProposedRoute();
+        if(teamWalks.size() == 0) {
+            findViewById(R.id.no_proposed_or_scheduled_walks_tv).setVisibility(View.VISIBLE);
+            return;
+        }
+
+        mCurrentTeamWalk = teamWalks.get(0);
+        displayAppropriateUIViewsForUser();
+    }
+
+    private void displayAppropriateUIViewsForUser() {
+        displayCommonUIViews();
+
+        // current user proposed the walk
+        if (mCurrentTeamWalk.getProposedBy().equals(mCurrentUser.getDisplayName())) {
+            displayProposerUIViews();
+        } else {
+            displayTeammateUIViews();
+        }
+    }
+
+    private void displayProposerUIViews() {
+
+    }
+
+    private void displayTeammateUIViews() {
+        LinearLayout teammateBtns = findViewById(R.id.schedule_propose_btns_accept_decline);
+        teammateBtns.setVisibility(View.VISIBLE);
+        displayProposedByViews();
+    }
+
+    private void displayProposedByViews() {
+        TextView proposedByPromptTv = findViewById(R.id.schedule_propose_tv_proposed_by_prompt);
+        proposedByPromptTv.setVisibility(View.VISIBLE);
+        TextView proposedByDisplayTv = findViewById(R.id.schedule_propose_tv_proposed_by_display);
+        proposedByDisplayTv.setVisibility(View.VISIBLE);
+        proposedByDisplayTv.setText(mCurrentTeamWalk.getProposedBy());
+    }
+
+    private void displayCommonUIViews() {
+        Route proposedRoute = mCurrentTeamWalk.getProposedRoute();
+        displayRouteDetails(proposedRoute);
+        displayProposedDateAndTime();
+    }
+
+    private void displayRouteDetails(Route proposedRoute) {
+        displayRouteName(proposedRoute);
+        displayRouteStartingLocation(proposedRoute);
+    }
+
+    private void displayRouteName(Route proposedRoute) {
+        TextView walkNamePrompt = findViewById(R.id.schedule_propose_tv_walk_name_prompt);
+        walkNamePrompt.setVisibility(View.VISIBLE);
+        TextView walkName = findViewById(R.id.schedule_propose_tv_walk_name_display);
+        walkName.setVisibility(View.VISIBLE);
         walkName.setText(proposedRoute.getTitle());
+    }
+
+    private void displayRouteStartingLocation(Route proposedRoute) {
+        TextView startingLocationPrompt = findViewById(R.id.schedule_propose_tv_starting_loc);
+        startingLocationPrompt.setVisibility(View.VISIBLE);
+        TextView startingLocation = findViewById(R.id.schedule_propose_tv_starting_loc_display);
+        startingLocation.setVisibility(View.VISIBLE);
         startingLocation.setText(proposedRoute.getStartingLocation());
-        String formattedDateAndTime = Utils.formatDateIntoReadableString("MM/dd/yyyy 'at' HH:mm a", teamWalks.get(0).getProposedDateAndTime().toDate());
+    }
+
+    private void displayProposedDateAndTime() {
+        TextView walkDatePrompt = findViewById(R.id.schedule_propose_tv_walk_date);
+        walkDatePrompt.setVisibility(View.VISIBLE);
+        TextView walkDate = findViewById(R.id.schedule_propose_tv_walk_date_display);
+        walkDate.setVisibility(View.VISIBLE);
+        String formattedDateAndTime = Utils.formatDateIntoReadableString("MM/dd/yyyy 'at' HH:mm a", mCurrentTeamWalk.getProposedDateAndTime().toDate());
         walkDate.setText(formattedDateAndTime);
     }
+
 }
